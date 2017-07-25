4634ce949b5c9b66823fdea5e64b32dfb0998cd3
Yhoarau <yaho26@hotmail.fr>
2017-07-25 02:08:10 +0200

    debug chat

--- src/php/function/chat.php
+++ src/php/function/chat.php
@@ -2,7 +2,8 @@
 
 function chat($db)
 {
-	$req_messages = $db->query("SELECT chat.id, pseudo, content FROM chat JOIN player WHERE chat.player = player.id AND room = '" . $_SESSION['idroom'] . "' AND player != " . $_SESSION['id'] " AND seen = 0 ORDER BY date_create ASC ");
+	$req_messages = $db->query("SELECT chat.id, pseudo, content FROM chat JOIN player WHERE chat.player = player.id AND room = '" . $_SESSION['idroom'] . "' AND player != " . $_SESSION['id'] . " AND seen = 0 ORDER BY date_create ASC ");
+	$messages = array();
 	for($i = 0;$data_messages = $req_messages->fetch(); $i++)
 	{
 		$messages[$i] = $data_messages;
